1d59847cf76f9103f4506545b75ad2a241dcfa3c
dhathri-9 <dhathri360@gmail.com>
2026-08-04 13:23:51 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          dark: '#1a1a1a',
+          light: '#fdfbf7',
+          gold: '#c5a059',
+          'gold-light': '#d4b77b',
+          gray: '#555555',
+        }
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
